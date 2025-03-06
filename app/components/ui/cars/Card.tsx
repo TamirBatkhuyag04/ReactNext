55d9d68e8579ec47892bd9484265fc613f678186
Tamir <tamir@htld>
2025-03-06 11:05:36 +0100

    Card.tsx isActive added

--- app/components/ui/cars/Card.tsx
+++ app/components/ui/cars/Card.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import { Car } from '@/app/lib/types'
+"use client"
+import React, { useState } from 'react'
+import { Car } from '@/lib/types'
 
 type Props = { car: Car }
 
 export default function Card({ car }: Props) {
+    const [isActive, setIsActive] = useState(false);
     return (
-        <div className="bg-green-100 border p-4">
-            {car.name}
+        <div
+            className={`bg-green-100 border p-4 hover:cursor-pointer ${isActive ? "bg-green-300" : "bg-gray-100"}`}
+            onClick={() => {
+                setIsActive(!isActive)
+            }}
+        >
+            <h2 className="font-bold text-3xl">{car.name}</h2>
+            <div>
+                {car.color}
+            </div>
+            {isActive ? "active" : "inactive"}
         </div>
     )
 }
